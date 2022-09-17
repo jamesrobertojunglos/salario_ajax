3ac5f7f1fe80695184180c49ffcd5a0003cbbe6e
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-09-17 20:35:30 -0300

    Exercicío acompanhado conforme Prof.Sandro

---
+++ salario.php
@@ -0,0 +1,30 @@
+<?php
+   define("salariominimo",1212);
+   $erro = false;
+   $msg = "";
+
+   function fNaoPreenchido($valor) {
+    if ($valor == "") {
+        return true;
+      } else {
+        return false;
+      }
+   }
+
+   if (fNaoPreenchido($_POST["iNome"])) {
+      $msg = "<p>Erro: Nome não informado</p>";
+      $erro = true;
+   }
+
+   if (fNaoPreenchido($_POST["iEndereco"])) {
+      $msg = $msg. "<p>Erro: Endereço não informado</p>";
+      $erro = true;
+   }
+
+   if (!$erro) {
+      printf("<strong>%s</strong> Recebe o equivalente a <strong>%1.2f salários mínimos</strong>. 
+      <br>Mora no endereço:<br>  <strong><pre>%s</pre></strong><br>",$_POST["iNome"],($_POST["iSalario"] / salariominimo),$_POST["iEndereco"]);
+   } else {
+   echo $msg;
+   }
+?>
